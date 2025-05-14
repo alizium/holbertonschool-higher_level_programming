b3ce48c213ddf4ffffd1dd59e6bba80561c6e23c
alizium <10549@holbertonstudents.com>
2025-05-14 14:36:47 +0200

    modifying

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     """Trie dans l'ordre alphabetique"""
     for key in sorted(a_dictionary.keys()):
-        print("{}".format(a_dictionary))
+        print("{}".format(key))
