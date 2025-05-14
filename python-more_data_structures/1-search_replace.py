5fe6a8fac95f52baac58c383d8eec0521dd19f42
alizium <10549@holbertonstudents.com>
2025-05-14 10:18:58 +0200

    adding commentary

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    Retourne une nouvelle liste en remplaçant 'search'
+    par 'replace', sans modifier la liste d'origine.
+    """
     new_list = []
     for x in my_list:
         if x == search:
             new_list.append(replace)
-    else:
-        new_list.append(x)
+        else:
+            new_list.append(x)
     return new_list
