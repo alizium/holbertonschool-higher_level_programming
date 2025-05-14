23eceffda1f3b311a7429ac2832270f3ac9ff701
alizium <10549@holbertonstudents.com>
2025-05-14 17:05:30 +0200

    new files

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-
     """retourne un dictionnaire,
     retourne la cle qui a la plus grande valeur
     si a_dictionary est vide, retourne none"""
-
-    # Si a_dictionary est vide
     if not a_dictionary:
-        # Retourne None
         return None
 
-    # On fait une boucle pour trouver la plus grande valeur
+    max_value = None
+    max_key = None
+
     for key, value in a_dictionary.items():
-
-        # On cree 2 variables
-        max_value = 0
-        max_key = None
-
-        if value > max_value:
+        if max_value is None or value > max_value:
             max_value = value
             max_key = key
 
