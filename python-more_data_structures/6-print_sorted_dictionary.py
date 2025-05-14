51ab5b3086163822100d7c536be667d128a0cca7
alizium <10549@holbertonstudents.com>
2025-05-14 14:22:44 +0200

    new files

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """Trie dans l'ordre alphabetique"""
+    for key in sorted(a_dictionary.keys()):
+        print("{}".format(a_dictionary))
