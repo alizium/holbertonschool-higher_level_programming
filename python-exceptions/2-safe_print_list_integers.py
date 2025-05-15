8842e7363a6791f03f9f9e1aa368a8bb2c24ef58
alizium <10549@holbertonstudents.com>
2025-05-15 16:54:22 +0200

    pycodestyle : except cannot be empty

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             variable_counter += 1
-        except:
+        except (ValueError, TypeError):
             pass
     print()
     return variable_counter
