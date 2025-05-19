d281b6f02b7b0485ac4438323010c47a3edc6e86
alizium <10549@holbertonstudents.com>
2025-05-19 17:37:31 +0200

    indentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""""Defines an empty Square class."""
+"""Defines an empty Square class."""
 
 class Square:
-"""empty class defines a square"""
+    """empty class defines a square"""
     pass
