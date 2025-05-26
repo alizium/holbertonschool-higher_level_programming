06075420f026e75f09ff1a3c02825780c347ffa8
alizium <10549@holbertonstudents.com>
2025-05-26 16:20:53 +0200

    lines

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-class MyClass1:
-
-    def lookup(obj):
-        """
-        fonction lookup : prend un objet en paramètre
-        et retourner la liste des attributs et des méthodes de cet objet
-        """
-        return dir(obj)
+def lookup(obj):
+    """
+    Fonction lookup : prend un objet en paramètre
+    et retourne la liste des attributs et des méthodes de cet objet.
+    """
+    return dir(obj)
