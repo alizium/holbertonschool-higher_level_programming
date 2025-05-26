6154889d8eb3203ed8f8e771d91acb66a99ae3b6
alizium <10549@holbertonstudents.com>
2025-05-26 19:05:27 +0200

    new files

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+
+    """
+    Vérifie si un objet est exactement une instance d'une classe donnée.
+
+    Cette fonction retourne True si l'objet obj est une instance directe
+    de la classe a_class (pas une sous-classe). Sinon, elle retourne False.
+
+    Paramètres :
+    obj -- l'objet à tester
+    a_class -- la classe à comparer avec l'objet
+
+    Retourne :
+    True si obj est exactement une instance de a_class, False sinon.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
