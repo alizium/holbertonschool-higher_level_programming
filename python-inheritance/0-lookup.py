91928993b95217bb16cef90066fa935223849160
alizium <10549@holbertonstudents.com>
2025-05-26 16:27:24 +0200

    ...

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-"""..."""
+
+"""
+Fonction lookup : prend un objet en paramètre
+et retourne la liste des attributs et des méthodes de cet objet.
+"""
 
 def lookup(obj):
-    """
-    Fonction lookup : prend un objet en paramètre
-    et retourne la liste des attributs et des méthodes de cet objet.
-    """
+
     return dir(obj)
