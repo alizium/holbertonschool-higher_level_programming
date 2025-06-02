609300b6a3d06bab5dd2f23a8384d047fde9ee2d
alizium <10549@holbertonstudents.com>
2025-06-02 17:03:47 +0200

    new files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+
+    """Ouvrir le fichier en mode lecture ('r') avec l'encodage 'utf-8' en utilisant le with statement
+    Donne un nom à ce fichier ouvert, comme f"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+
+        # lis et retourne le contenu du fichier f.read
+        print(f.read())
