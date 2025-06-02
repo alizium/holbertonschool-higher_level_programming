7a5f8a0ffcac0335dc614005913cfb448de051b9
alizium <10549@holbertonstudents.com>
2025-06-02 17:06:30 +0200

    removing '\n'

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
 
         # lis et retourne le contenu du fichier f.read
-        print(f.read())
+        print(f.read(), end="")
