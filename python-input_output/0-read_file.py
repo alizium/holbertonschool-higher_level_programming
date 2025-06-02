24ed3d3a9ec415f5fdac255a525e0ef57fcc5131
alizium <10549@holbertonstudents.com>
2025-06-02 17:08:50 +0200

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,9 +2,11 @@
 
 """permet de lire le fichier filename"""
 
+
 def read_file(filename=""):
 
-    """Ouvrir le fichier en mode lecture ('r') avec l'encodage 'utf-8' en utilisant le with statement
+    """Ouvrir le fichier en mode lecture ('r')
+    avec l'encodage 'utf-8' en utilisant le with statement
     Donne un nom à ce fichier ouvert, comme f"""
 
     with open(filename, "r", encoding="utf-8") as f:
