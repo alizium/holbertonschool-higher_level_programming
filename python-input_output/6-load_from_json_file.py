20c970cc66f594d255dd3d934c707d51257865bf
alizium <10549@holbertonstudents.com>
2025-06-07 17:53:43 +0200

    documenntation

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
+"""import de json"""
+
 import json
+
+"""Lit un fichier JSON et retourne l'objet correspondant"""
+
 
 def load_from_json_file(filename):
 
+    """Ouvre le fichier en lecture ("r") avec encodage UTF-8"""
     with open(filename, "r", encoding="utf-8") as f:
 
+        """Lit tout le contenu du fichier sous forme de chaîne"""
         lire_fichier = f.read()
+        """Convertit la chaîne JSON en objet Python et le retourne"""
         return json.loads(lire_fichier)
