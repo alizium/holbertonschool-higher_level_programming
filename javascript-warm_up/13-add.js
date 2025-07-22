58cebd6d8467e9e33b8f58fe064b155e5a5d625c
alizium <10549@holbertonstudents.com>
2025-07-22 14:31:05 +0200

    update

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const add = require('./13-add').add;
 console.log(add(3, 5));  // Affiche 8
-
-// on définis une fonction "add" qui additionne deux nombres
-exports.add = function (a, b) {
-  return a + b;
-};
