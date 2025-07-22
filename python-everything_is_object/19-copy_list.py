49220ffce0bf4e57c67016d6c723792ff3a6d1e2
alizium <10549@holbertonstudents.com>
2025-07-22 09:45:26 +0200

    line

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-def copy_list(a_list):     # Déclare une fonction qui prend une liste en argument
-    return a_list[:]       # Renvoie une copie de la liste via slicing (copie superficielle)
-
+def copy_list(a_list):
+    return list(a_list)
